ebe75b13d26cc4c5c10b5e6b9fc9f3c513d3596b
S-L-A-Y-E-R <elshehry2000@gmail.com>
2025-06-09 14:24:37 +0300

    commit

--- learn/src/main/java/com/learn/pojo/Name.java
+++ learn/src/main/java/com/learn/pojo/Name.java
@@ -1,8 +1,35 @@
 package com.learn.pojo;
 
+import com.learn.customAnnotation.CourseCode;
+import jakarta.validation.constraints.*;
+
 public class Name {
+    @NotNull(message = "required")
+    @Size(min = 1,message = "required")
     private String firstName;
+
+    @NotNull(message = "required")
+    @Size(min = 1,message = "required")
     private String lastName;
+
+    @NotNull(message = "required")
+    private String country;
+
+    @NotNull(message = "required")
+    private String language;
+
+    @Min(value = 0,message = "The value must be between 0 and 10")
+    @Max(value = 10,message = "The value must be between 0 and 10")
+    @NotNull(message = "required")
+    private Integer freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}",message = "only 5 digits/characters")
+    private String postalCode;
+
+    @CourseCode(value = "LOV" , message = "must start with LOV")
+    private String courseCode;
+
+    private String os;
 
     // Getters and setters
     public String getFirstName() {
@@ -19,5 +46,53 @@ public class Name {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
     }
 }
--- learn/src/main/java/com/learn/rest/DemoController.java
+++ learn/src/main/java/com/learn/rest/DemoController.java
@@ -2,14 +2,22 @@ package com.learn.rest;
 
 import com.learn.Coach;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import com.learn.pojo.Name;
 
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class DemoController {
@@ -44,13 +52,24 @@ public class DemoController {
     }
 
     @GetMapping("/showForm")
-    public String showForm(Model model){
+    public String showForm(Model model,@Value("${countries}") List<String> countries){
         model.addAttribute("name",new Name());
+        model.addAttribute("countries",countries);
         return "helloWorldForm";
     }
 
     @PostMapping("/processForm")
-    public String processFom( @ModelAttribute("name") Name name){
-        return "processForm";
+    public String processFom(@Valid @ModelAttribute("name") Name name, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "helloWorldForm";
+        }else {
+            return "processForm";
+        }
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder){
+        StringTrimmerEditor trimmerEditor = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class,trimmerEditor);
     }
 }
