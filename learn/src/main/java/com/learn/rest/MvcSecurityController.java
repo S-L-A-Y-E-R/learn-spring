7d8d196e411fe60988b0fb900725b8bb38deac16
S-L-A-Y-E-R <elshehry2000@gmail.com>
2025-06-13 13:34:14 +0300

    commit

---
+++ learn/src/main/java/com/learn/rest/MvcSecurityController.java
@@ -0,0 +1,32 @@
+package com.learn.rest;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MvcSecurityController {
+
+    @GetMapping("/")
+    public String homePage(Model model){
+        model.addAttribute("role", SecurityContextHolder.getContext()
+                        .getAuthentication().getAuthorities().stream().
+                        map(GrantedAuthority::getAuthority).
+                        findFirst().orElse("")
+                );
+        return "home-page";
+    }
+
+    @GetMapping("/managers")
+    public  String managersPage(){
+        return "managers-page";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login-page";
+    }
+
+}
